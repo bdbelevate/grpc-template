64376b35418e6088036763560a50d7cd72056183
Brian DeBoer <bdeboer@experiencecosm.com>
2020-12-08 14:21:25 -0800

    fix build.rs

--- build.rs
+++ build.rs
@@ -1,14 +1,17 @@
+{% assign name = crate_name | remove: "_service" %}
+{% assign pascal = name | pascal_case %}
+
 use prost_build::Config;
 
 fn main() {
     let mut config = Config::new();
     // config.compile_well_known_types();
-    config.type_attribute("Sample", "#[derive(serde::Serialize, serde::Deserialize)]");
+    config.type_attribute("{{pascal}}", "#[derive(serde::Serialize, serde::Deserialize)]");
     config.type_attribute(
         "Timestamp",
         "#[derive(serde::Serialize, serde::Deserialize)]",
     );
-    config.field_attribute("Sample.id", "#[serde(rename = \"_id\", serialize_with = \"crate::db::id::serialize\", deserialize_with = \"crate::db::id::deserialize\")]");
+    config.field_attribute("{{pascal}}.id", "#[serde(rename = \"_id\", serialize_with = \"crate::db::id::serialize\", deserialize_with = \"crate::db::id::deserialize\")]");
 
     tonic_build::configure()
         .build_client(false)
